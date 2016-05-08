aeb73bd02e45a71bdb438390efa3adde5660fb8c
giovannini <giovannini.thomas@gmail.com>
2016-05-08 21:56:31 +0200

    Problem 5

---
+++ src/main/scala/Ex5.scala
@@ -0,0 +1,12 @@
+/**
+  2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+  What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+  */
+object Ex5 extends App {
+
+  val result = 5 * 7 * 9 * 11 * 13 * 16 * 17 * 19
+
+  println(result)
+
+}
