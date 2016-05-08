7758ac4835ae17aee0fe1f73adda525911e13f5c
giovannini <giovannini.thomas@gmail.com>
2016-05-08 15:27:59 +0200

    Problem 1

---
+++ src/main/scala/Ex1.scala
@@ -0,0 +1,13 @@
+/**
+  * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+  * Find the sum of all the multiples of 3 or 5 below 1000.
+  */
+object Ex1 extends App {
+
+  val exResult = (1 until 10).filter(a => a % 3 == 0 || a % 5 == 0).sum
+  println(exResult)
+
+  val result = (1 until 1000).filter(a => a % 3 == 0 || a % 5 == 0).sum
+  println(result)
+
+}
